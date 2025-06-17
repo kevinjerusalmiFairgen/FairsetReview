06ff2f51aa145b649c1f27d1eadac81c43aef9bf
kevin <kevin.jerusalmi@fairgen.ai>
2025-06-17 12:36:26 +0300

    m

--- app.py
+++ app.py
@@ -85,6 +85,7 @@ def main():
     with tab1: 
         st.markdown("Upload train set, fairset and prior file")
         japanase_chars = st.checkbox("Japanese survey")
+        japanase_chars_fairset = st.checkbox("Japanese fairset")
 
 
         if st.button("Run Analysis"):
@@ -92,7 +93,7 @@ def main():
                 st.warning("Upload train, fairset and prior file before running analysis!")
             if train_file is not None and fairset_file is not None and priorfile_file is not None:
                 train = load_file(train_file, japanase_chars)
-                fairset = load_file(fairset_file, japanase_chars)
+                fairset = load_file(fairset_file, japanase_chars_fairset)
                 priorfile = load_file(priorfile_file)
 
                 # Check columns are all right
